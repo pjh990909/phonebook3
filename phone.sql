8faee5f85d98282406d8883355da72eabcf7ac4f
pjh990909 <pjh990909@naver.com>
2024-02-29 15:49:56 +0900

    2/29

--- phone.sql
+++ phone.sql
@@ -12,8 +12,17 @@ VALUES (null,'박종희', '010-1111-1111', '02-555-5555');
 select *
 from person;
 
-select person_id,
+select person_id as personId,
 	   name,
        hp,
        company
 from person;
+
+delete from person
+where person_id = 18;
+
+update person
+set name = '기안' ,
+	hp = '01015555',
+    company = '05-8848'
+where person_id = 3 ;
